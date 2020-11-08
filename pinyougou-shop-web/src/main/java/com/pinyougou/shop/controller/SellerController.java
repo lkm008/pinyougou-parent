8c54218ca13eb9f63134f95524475ad35f05e51a
lkm008 <1524117229@qq.com>
2020-11-08 14:19:05 +0800

    5.4  密码加密 5.4.2商家入驻密码加密

--- pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/SellerController.java
+++ pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/SellerController.java
@@ -5,6 +5,7 @@ import com.pinyougou.entity.PageResult;
 import com.pinyougou.entity.Result;
 import com.pinyougou.pojo.TbSeller;
 import com.pinyougou.sellergoods.service.SellerService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +50,10 @@ public class SellerController {
 	 */
 	@RequestMapping("/add")
 	public Result add(@RequestBody TbSeller seller){
+		//密码加密
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String password = passwordEncoder.encode(seller.getPassword());
+		seller.setPassword(password);
 		try {
 			seller.setStatus("0");
 			seller.setCreateTime(new Date());
